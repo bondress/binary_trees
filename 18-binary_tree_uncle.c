39198215e7af50c7f771e7ed04bc59fe6636641a
Laide Endeley <endeleylaide@gmail.com>
2023-10-05 14:11:11 -0700

    Attempting to fix errors

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,7 +9,7 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node->parent || !node)
+	if (!node || !node->parent)
 		return (NULL);
 
 	return (binary_tree_sibling(node->parent));
@@ -24,7 +24,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node->parent || !node)
+	if (!node || !node->parent)
 		return (NULL);
 
 	if (node == node->parent->left)
